a42ff58e34a5feb2fd552dc48f1e2762e15b121b
BolNick <BolNick09RUS@gmail.com>
2025-04-06 00:46:52 +0300

    +(TestPrj): Тестовый код для проверки подключения БД

--- TestPrj/Program.cs
+++ TestPrj/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace TestPrj
+﻿using System;
+using CommonData;
+
+namespace TestPrj
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var db = new CookBookDbContext();
+
+            // Проверка, что БД создана и таблицы существуют
+            db.Database.EnsureCreated();
+
+            // Пример добавления тестовых данных
+            if (!db.Products.Any())
+            {
+                db.Products.Add(new Product { Name = "Яблоко", Proteins = 3, Fats = 2, Carbohydrates = 14, Calories = 52 });
+                db.SaveChanges();
+                Console.WriteLine("Добавлен тестовый продукт!");
+            }
+
+            Console.WriteLine($"Продуктов в БД: {db.Products.Count()}");
         }
     }
 }
