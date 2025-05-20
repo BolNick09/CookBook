c31d1ee77108c1ac0cf4f1ede7b6c1e26206e052
George Dovlatyanc <dovlatyanc@list.ru>
2025-05-20 20:22:17 +0300

    +Добавил MainWindow

--- Client_CookBookApp/MainWindow.xaml.cs
+++ Client_CookBookApp/MainWindow.xaml.cs
@@ -9,16 +9,59 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Client_CookBookApp
+namespace Client_CookBookApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    private void OpenSearchRecipes_Click(object sender, RoutedEventArgs e)
+    {
+        var searchWindow = new SearchRecipesWindow();
+        searchWindow.Owner = this;
+        searchWindow.Show();
+    }
+
+    private void OpenRecipesManagement_Click(object sender, RoutedEventArgs e)
+    {
+        var recipesWindow = new RecipesManagementWindow();
+        recipesWindow.Owner = this;
+        recipesWindow.Show();
+    }
+
+    private void OpenProductsManagement_Click(object sender, RoutedEventArgs e)
+    {
+        var productsWindow = new ProductsManagementWindow();
+        productsWindow.Owner = this;
+        productsWindow.Show();
+    }
+
+    private void OpenCalorieCalculator_Click(object sender, RoutedEventArgs e)
+    {
+        var calculatorWindow = new CalorieCalculatorWindow();
+        calculatorWindow.Owner = this;
+        calculatorWindow.Show();
+    }
+
+    private void favorite_Click(object sender, RoutedEventArgs e)
+    {
+        var favoriteWindow = new Favorite();
+        favoriteWindow.Owner = this;
+        favoriteWindow.Show();
+    }
+
+    private void OpenProductsSearch_Click(object sender, RoutedEventArgs e)
+    {
+
+        var window = new CombinedSearchWindow();
+        window.Owner = this;
+        window.Show();
+
     }
 }
