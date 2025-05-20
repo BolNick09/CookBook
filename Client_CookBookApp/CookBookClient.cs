16cab0b76da92a916a2190e1436d6c6ecfd92169
George Dovlatyanc <dovlatyanc@list.ru>
2025-05-20 21:18:53 +0300

    +Добавил класс для взаимодействия с сервером и измени аппдату

---
+++ Client_CookBookApp/CookBookClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_CookBookApp;
+
+
+    public class CookBookClient
+    {
+        private readonly ServerCommonData.CookBookClient _client;
+
+        public CookBookClient()
+        {
+            _client = new ServerCommonData.CookBookClient("127.0.0.1", 2024);
+        }
+
+        public async Task<T> SendRequest<T>(string action, string entityType, object data = null)
+        {
+            return await _client.SendRequestAsync<T>(action, entityType, data);
+        }
+    }
+
