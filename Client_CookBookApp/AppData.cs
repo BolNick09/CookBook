b22873ff18b5e3fe9480eadf8d17edc9c6656fb2
George Dovlatyanc <dovlatyanc@list.ru>
2025-05-20 21:13:39 +0300

    Рабочий вариант без сервера

--- Client_CookBookApp/AppData.cs
+++ Client_CookBookApp/AppData.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommonData;
+using Microsoft.EntityFrameworkCore;
 
 namespace Client_CookBookApp;
 
--- Client_CookBookApp/RecipesManagementWindow.xaml.cs
+++ Client_CookBookApp/RecipesManagementWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using CommonData;
+using Microsoft.EntityFrameworkCore;
 
 namespace Client_CookBookApp
 {
